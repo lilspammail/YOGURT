cffa4be0e96c188bf9d45574dbb8976f27616f8e
lilspammail <lilspammail20@gmail.com>
2025-06-05 01:43:08 +0300

    fix shortcut list builder

--- YOGURT/HealthAppShortcuts.swift
+++ YOGURT/HealthAppShortcuts.swift
@@ -2,15 +2,13 @@ import AppIntents
 
 struct HealthAppShortcuts: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
-        [
-            AppShortcut(
-                intent: SendHealthDataIntent(),
-                phrases: [
-                    AppShortcutPhrase("Отправить данные с ${applicationName}")
-                ],
-                shortTitle: "Синхронизация",
-                systemImageName: "arrow.up.circle.fill"
-            )
-        ]
+        AppShortcut(
+            intent: SendHealthDataIntent(),
+            phrases: [
+                AppShortcutPhrase("Отправить данные с ${applicationName}")
+            ],
+            shortTitle: "Синхронизация",
+            systemImageName: "arrow.up.circle.fill"
+        )
     }
 }
